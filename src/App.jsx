6cfd5fde61df135517961449d8b7de4b837e8535
DevSteg <j.l.stephens91@gmail.com>
2022-05-19 13:44:02 +0100

    Initial commit with navbar

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import NavbarTop from "./components/NavbarTop";
+
+function App() {
+	return (
+		<div className="App">
+			<header className="App-header">
+				<NavbarTop />
+			</header>
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/NavbarTop.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Navbar, Container, Nav, NavDropdown } from "react-bootstrap";
+import logo from "../assets/images/logo.png";
+
+const NavbarTop = () => {
+	return (
+		<Navbar bg="primary" expand="lg">
+			<Container>
+				<Navbar.Brand href="#home">
+					<img src={logo} alt="Logo" className="logo-flower" />
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls="basic-navbar-nav" />
+				<Navbar.Collapse id="basic-navbar-nav">
+					<Nav className="me-auto">
+						<NavDropdown title="Shop" id="basic-nav-dropdown">
+							<NavDropdown.Item href="#products">
+								<h6 className="dropdown-item">All Products</h6>
+							</NavDropdown.Item>
+							<NavDropdown.Divider />
+							<NavDropdown.Item href="#products/dishes">
+								<h6 className="dropdown-item">Dishes</h6>
+							</NavDropdown.Item>
+							<NavDropdown.Item href="#products/cups">
+								<h6 className="dropdown-item">Cups</h6>
+							</NavDropdown.Item>
+							<NavDropdown.Item href="#products/planters">
+								<h6 className="dropdown-item">Planters</h6>
+							</NavDropdown.Item>
+						</NavDropdown>
+						<Nav.Link href="#home">
+							<h6 className="nav-item">Home</h6>
+						</Nav.Link>
+						<Nav.Link href="#about">
+							<h6 className="nav-item">About</h6>
+						</Nav.Link>
+						<Nav.Link href="#login">
+							<h6 className="nav-item">Login</h6>
+						</Nav.Link>
+						<Nav.Link href="#signup">
+							<h6 className="nav-item">Sign Up</h6>
+						</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Container>
+		</Navbar>
+	);
+};
+
+export default NavbarTop;
