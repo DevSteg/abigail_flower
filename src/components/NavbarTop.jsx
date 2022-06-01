02e9e5da1b4a37bac65d8f7f3795b2195de05400
DevSteg <j.l.stephens91@gmail.com>
2022-06-01 20:38:42 +0100

    Add link locations to navbar links

--- src/components/NavbarTop.jsx
+++ src/components/NavbarTop.jsx
@@ -6,37 +6,37 @@ const NavbarTop = () => {
 	return (
 		<Navbar bg="primary" expand="lg">
 			<Container>
-				<Navbar.Brand href="#home">
+				<Navbar.Brand href="/">
 					<img src={logo} alt="Logo" className="logo-flower" />
 				</Navbar.Brand>
 				<Navbar.Toggle aria-controls="basic-navbar-nav" />
 				<Navbar.Collapse id="basic-navbar-nav">
 					<Nav className="me-auto">
 						<NavDropdown title="Shop" id="basic-nav-dropdown">
-							<NavDropdown.Item href="#products">
+							<NavDropdown.Item href="/products">
 								<h6 className="dropdown-item">All Products</h6>
 							</NavDropdown.Item>
 							<NavDropdown.Divider />
-							<NavDropdown.Item href="#products/dishes">
+							<NavDropdown.Item href="/products/dishes">
 								<h6 className="dropdown-item">Dishes</h6>
 							</NavDropdown.Item>
-							<NavDropdown.Item href="#products/cups">
+							<NavDropdown.Item href="/products/cups">
 								<h6 className="dropdown-item">Cups</h6>
 							</NavDropdown.Item>
-							<NavDropdown.Item href="#products/planters">
+							<NavDropdown.Item href="/products/planters">
 								<h6 className="dropdown-item">Planters</h6>
 							</NavDropdown.Item>
 						</NavDropdown>
-						<Nav.Link href="#home">
+						<Nav.Link href="/">
 							<h6 className="nav-item">Home</h6>
 						</Nav.Link>
-						<Nav.Link href="#about">
+						<Nav.Link href="/about">
 							<h6 className="nav-item">About</h6>
 						</Nav.Link>
-						<Nav.Link href="#login">
+						<Nav.Link href="/login">
 							<h6 className="nav-item">Login</h6>
 						</Nav.Link>
-						<Nav.Link href="#signup">
+						<Nav.Link href="/signup">
 							<h6 className="nav-item">Sign Up</h6>
 						</Nav.Link>
 					</Nav>
